d18f114407ce8d1cdd7bd4ea71c3c2dd42d1d2f4
Ian-Jay <ianjaymamansag995@gmail.com>
2021-08-17 12:49:53 +0000

    bug fix

--- internal/redis/role.go
+++ internal/redis/role.go
@@ -12,10 +12,10 @@ func (t *RBAC) RoleCreated(ctx context.Context, roles internal.Roles) error {
 
 // Deleted publishes a message indicating a roles was deleted.
 func (t *RBAC) RoleDeleted(ctx context.Context, id string) error {
-	return t.publish(ctx, "Role.Deleted", internal.EVENT_ROLETASK_DELETED, id)
+	return t.publish(ctx, "Role.Deleted", internal.EVENT_ROLE_DELETED, id)
 }
 
 // Updated publishes a message indicating a roles was updated.
 func (t *RBAC) RoleUpdated(ctx context.Context, role internal.Roles) error {
-	return t.publish(ctx, "Role.Updated", internal.EVENT_ROLETASK_UPDATED, role)
+	return t.publish(ctx, "Role.Updated", internal.EVENT_ROLE_UPDATED, role)
 }
